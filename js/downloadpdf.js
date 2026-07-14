2ff0aaa851aa1797b169c9ad402246633bda1a50
Mohammad Sayeed <khansayeed25519@gmail.com>
2026-07-14 12:49:26 +0530

    layout and content changes

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -1,21 +1,9 @@
 function downloadPDF(event) {
   event.preventDefault(); // Prevent the default behavior of the link
 
-  // Path to your PDF file
-  var pdfURL = "./document/resume.pdf";
+  // Resume hosted on Google Drive
+  var driveURL =
+    "https://drive.google.com/drive/folders/12vEQZYYmn8Yg4AH77DNZ1EDh8KB9P1yh?usp=drive_link";
 
-  // Create a temporary link element
-  var link = document.createElement("a");
-  link.href = pdfURL;
-  link.target = "_blank"; // Open in a new tab
-  link.download = "Resume.pdf"; // File name when downloaded
-
-  // Append the link to the body
-  document.body.appendChild(link);
-
-  // Trigger the click event of the link
-  link.click();
-
-  // Clean up
-  document.body.removeChild(link);
+  window.open(driveURL, "_blank");
 }
